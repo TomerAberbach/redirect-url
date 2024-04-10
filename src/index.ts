ef731df9554c6a3fd093cceb8c5b001c1f01d6d7
Tomer Aberbach <tomeraberbach@gmail.com>
2024-04-09 22:12:28 -0400

    feat: implement!

--- src/index.ts
+++ src/index.ts
@@ -14,6 +14,276 @@
  * limitations under the License.
  */
 
-const redirectUrl = (): string => `Hello World!`
+import {
+  compile as createParamsToPathFunction,
+  match as createPathToParamsFunction,
+} from 'path-to-regexp'
+import type { ReadonlyDeep, RequiredDeep } from 'type-fest'
 
-export default redirectUrl
+/**
+ * Compiles and returns a {@link RedirectUrl} function from a redirects
+ * specification string.
+ *
+ * For performance, do not call this function before each redirect computation.
+ * Compile the {@link RedirectUrl} function once and reuse it.
+ *
+ * The syntax of the string loosely matches the syntax of Netlify's
+ * `_redirects` file:
+ * https://docs.netlify.com/routing/redirects#syntax-for-the-redirects-file
+ *
+ * Each line of the string can be either:
+ * - Whitespace and/or comment (starting with `#`), in which case it the line is
+ *   ignored.
+ * - Whitespace separated {@link ObjectRedirect#from},
+ *   {@link ObjectRedirect#to}, and optionally {@link ObjectRedirect#status}.
+ *
+ * See {@link Redirect} for how to specify redirects and
+ * {@link RedirectUrlOptions} for options.
+ *
+ * @example
+ * ```
+ * # Nice :)
+ * /bliss  https://www.youtube.com/watch?v=dQw4w9WgXcQ
+ *
+ * # Other redirects...
+ * /a      /b
+ * /x/y/z  /a/b/c
+ * ```
+ */
+export const parseRedirectUrl = (
+  redirects: string,
+  options?: ReadonlyDeep<RedirectUrlOptions>,
+): RedirectUrl =>
+  createRedirectUrl(
+    redirects.split(`\n`).flatMap(line => {
+      const redirect = parseRedirect(line)
+      return redirect ? [redirect] : []
+    }),
+    options,
+  )
+
+const parseRedirect = (line: string): Redirect | null => {
+  line = line.trim()
+  if (!line.length || line.startsWith(`#`)) {
+    return null
+  }
+
+  const parts = line.split(WHITESPACE_REGEX)
+  if (!(parts.length >= 2 && parts.length <= 3)) {
+    throw new Error(`Bad part count:\n${line}`)
+  }
+
+  const [from, to, rawStatus] = parts
+  let status
+  if (rawStatus) {
+    status = parseInt(rawStatus, 10)
+    if (isNaN(status)) {
+      throw new TypeError(`Bad status: ${rawStatus}`)
+    }
+  }
+
+  return { from: from!, to: to!, status }
+}
+
+const WHITESPACE_REGEX = /\s+/u
+
+/**
+ * Compiles and returns a {@link RedirectUrl} function.
+ *
+ * For performance, do not call this function before each redirect computation.
+ * Compile the {@link RedirectUrl} function once and reuse it.
+ *
+ * See {@link Redirect} for how to specify redirects and
+ * {@link RedirectUrlOptions} for options.
+ */
+export const createRedirectUrl = (
+  redirects: ReadonlyDeep<Redirect[]>,
+  {
+    defaultStatus = 301,
+    encode = encodeURIComponent,
+    decode = decodeURIComponent,
+  }: ReadonlyDeep<RedirectUrlOptions> = {},
+): RedirectUrl => {
+  const options = { defaultStatus, encode, decode }
+  const compiledRedirects = redirects.map(redirect =>
+    compileRedirect(redirect, options),
+  )
+  return inputUrl => {
+    const url = new URL(inputUrl)
+    for (const redirect of compiledRedirects) {
+      const newUrl = redirect.transform(url)
+      if (newUrl) {
+        return { url: newUrl, status: redirect.status }
+      }
+    }
+    return null
+  }
+}
+
+const compileRedirect = (
+  redirect: ReadonlyDeep<Redirect>,
+  {
+    defaultStatus,
+    encode,
+    decode,
+  }: ReadonlyDeep<RequiredDeep<RedirectUrlOptions>>,
+): CompiledRedirect => {
+  const { from, to, status = defaultStatus } = normalizeRedirect(redirect)
+  if (!isPath(from)) {
+    throw new Error(`Bad from path: ${from}`)
+  }
+
+  const pathToParams = createPathToParamsFunction(getPath(from), { decode })
+  const paramsToPath = createParamsToPathFunction(getPath(to), { encode })
+
+  return {
+    transform: url => {
+      const params = (pathToParams(getPath(url)) || null)?.params
+      if (!params) {
+        return null
+      }
+
+      const newUrl = new URL(to, url)
+      newUrl.pathname = paramsToPath(params)
+      for (const [name, value] of url.searchParams) {
+        newUrl.searchParams.append(name, value)
+      }
+      if (url.hash) {
+        newUrl.hash = url.hash
+      }
+      return String(newUrl)
+    },
+    status,
+  }
+}
+
+type CompiledRedirect = {
+  transform: (url: URL) => string | null
+  status: number
+}
+
+const normalizeRedirect = (
+  redirect: ReadonlyDeep<Redirect>,
+): ReadonlyDeep<ObjectRedirect> => {
+  if (`from` in redirect) {
+    return redirect
+  }
+
+  const [from, to] = redirect
+  return { from, to }
+}
+
+const isPath = (url: string): boolean => createUrl(url).pathname === url
+
+const getPath = (url: string | URL): string =>
+  removeTrailingSlash(createUrl(url).pathname)
+
+const createUrl = (url: string | URL): URL =>
+  new URL(url, `https://tomeraberba.ch`)
+
+const removeTrailingSlash = (url: string): string =>
+  url.endsWith(`/`) ? url.slice(0, -1) : url
+
+/**
+ * An object that specifies a redirect from a path pattern to a URL, optionally
+ * with a status code for the redirection.
+ *
+ * See `ObjectRedirect` for more information.
+ */
+export type Redirect = TupleRedirect | ObjectRedirect
+
+/**
+ * A pair of strings that specifies a redirect from a path pattern (the first
+ * string) to a URL (the second string).
+ *
+ * See `ObjectRedirect` for more information.
+ */
+export type TupleRedirect = [string, string]
+
+/**
+ * An object that specifies a redirect from a path pattern to a URL, optionally
+ * with a status code for the redirection.
+ */
+export type ObjectRedirect = {
+  /**
+   * A string specifying the pattern a URL's path must match for this redirect
+   * to apply to it.
+   *
+   * The pattern must be a path (no host, query params, fragment, etc.) that
+   * follows `path-to-regexp`'s syntax:
+   * https://github.com/pillarjs/path-to-regexp#parameters
+   *
+   * Matching is case-insensitive and ignores trailing slashes.
+   */
+  from: string
+
+  /**
+   * A string specifying the URL to redirect to.
+   *
+   * The string may use parameters of the {@link ObjectRedirect#from} path
+   * pattern according to `path-to-regexp`'s syntax:
+   * https://github.com/pillarjs/path-to-regexp#match
+   *
+   * The string may be a path or a full URL. For the former, the URL returned by
+   * {@link RedirectUrl} will use the host of the matching input URL. In both
+   * cases, any query params or fragments present on an input URL matching the
+   * {@link ObjectRedirect#from} pattern are carried over to the redirect URL.
+   *
+   * Query params are always appended, even for duplicate query param names,
+   * while the fragments always overwrite.
+   */
+  to: string
+
+  /**
+   * The status code to use for the redirection.
+   *
+   * Defaults to {@link RedirectUrlOptions#defaultStatus} if not specified.
+   */
+  status?: number
+}
+
+/**
+ * The options object for creating a {@link RedirectUrl} function.
+ *
+ * All properties are optional.
+ */
+export type RedirectUrlOptions = {
+  /**
+   * The default status to use for redirects when no particular status is
+   * specified.
+   *
+   * Defaults to 301 ("Moved Permanently").
+   */
+  defaultStatus?: number
+
+  /**
+   * A function for encoding text as a valid component of a URI.
+   *
+   * Defaults to {@link encodeURIComponent}.
+   */
+  encode?: (component: string) => string
+
+  /**
+   * A function for decoding text that was decoded using
+   * {@link RedirectUrlOptions#encode}.
+   *
+   * Defaults to {@link decodeURIComponent}.
+   */
+  decode?: (component: string) => string
+}
+
+/** The result of computing a redirect for a URL. */
+export type RedirectUrlResult = {
+  /** The URL to redirect to. */
+  url: string
+
+  /** The status code of the redirection. */
+  status: number
+}
+
+/**
+ * A function that returns the URL to redirect the input `url` to, along with a
+ * status code for the redirection, or null if no mapping exists for the input
+ * URL.
+ */
+export type RedirectUrl = (url: string) => RedirectUrlResult | null
--- test/index.ts
+++ test/index.ts
@@ -14,10 +14,131 @@
  * limitations under the License.
  */
 
-import { expectTypeOf } from 'tomer'
-import redirectUrl from '../src/index.js'
+import { createRedirectUrl, parseRedirectUrl } from '../src/index.ts'
+import type { RedirectUrlResult } from '../src/index.ts'
 
-test(`redirectUrl works`, () => {
-  expectTypeOf(redirectUrl).toEqualTypeOf<() => string>()
-  expect(redirectUrl()).toBe(`Hello World!`)
+test.each([`https://tomeraberba.ch`, `/a?`, `/a?x=2`, `/a#`, `/a#x`])(
+  `createRedirectUrl - bad from path %p`,
+  from => {
+    expect(() => createRedirectUrl([[from, `/`]])).toThrow(
+      new Error(`Bad from path: ${from}`),
+    )
+  },
+)
+
+test.each([
+  [`https://tomeraberba.ch`, [`https://tomeraberba.ch/0`, 301]],
+  [`https://tomeraberba.ch/`, [`https://tomeraberba.ch/0`, 301]],
+
+  [`https://tomeraberba.ch/a`, [`https://tomeraberba.ch/1`, 301]],
+  [`https://tomeraberba.ch/a/`, [`https://tomeraberba.ch/1`, 301]],
+
+  [`https://tomeraberba.ch/b`, [`https://tomeraberba.ch/2`, 301]],
+  [`https://tomeraberba.ch/b/`, [`https://tomeraberba.ch/2`, 301]],
+
+  [`https://tomeraberba.ch/c`, [`https://tomeraberba.ch/3`, 301]],
+  [`https://tomeraberba.ch/c/`, [`https://tomeraberba.ch/3`, 301]],
+
+  [`https://tomeraberba.ch/d`, [`https://tomeraberba.ch/4`, 301]],
+  [`https://tomeraberba.ch/d/`, [`https://tomeraberba.ch/4`, 301]],
+
+  [`https://tomeraberba.ch/e`, null],
+  [`https://tomeraberba.ch/e/`, null],
+
+  [`https://tomeraberba.ch/e/x`, [`https://tomeraberba.ch/5/x`, 301]],
+  [`https://tomeraberba.ch/e/x/`, [`https://tomeraberba.ch/5/x`, 301]],
+
+  [`https://tomeraberba.ch/x`, [`https://tomeraberba.ch/y`, 307]],
+  [`https://tomeraberba.ch/x/`, [`https://tomeraberba.ch/y`, 307]],
+
+  [`https://tomeraberba.ch?a=b`, [`https://tomeraberba.ch/0?a=b`, 301]],
+  [`https://tomeraberba.ch/?a=b`, [`https://tomeraberba.ch/0?a=b`, 301]],
+  [`https://tomeraberba.ch#x`, [`https://tomeraberba.ch/0#x`, 301]],
+  [`https://tomeraberba.ch/#x`, [`https://tomeraberba.ch/0#x`, 301]],
+  [`https://tomeraberba.ch?a=b#x`, [`https://tomeraberba.ch/0?a=b#x`, 301]],
+  [`https://tomeraberba.ch/?a=b#x`, [`https://tomeraberba.ch/0?a=b#x`, 301]],
+
+  [`https://tomeraberba.ch/f`, [`https://tomeraberba.ch/6?a=b`, 301]],
+  [`https://tomeraberba.ch/f/`, [`https://tomeraberba.ch/6?a=b`, 301]],
+  [`https://tomeraberba.ch/f?a=c`, [`https://tomeraberba.ch/6?a=b&a=c`, 301]],
+  [`https://tomeraberba.ch/f/?a=c`, [`https://tomeraberba.ch/6?a=b&a=c`, 301]],
+  [`https://tomeraberba.ch/f?x=y`, [`https://tomeraberba.ch/6?a=b&x=y`, 301]],
+  [`https://tomeraberba.ch/f/?x=y`, [`https://tomeraberba.ch/6?a=b&x=y`, 301]],
+  [`https://tomeraberba.ch/f#x`, [`https://tomeraberba.ch/6?a=b#x`, 301]],
+  [`https://tomeraberba.ch/f/#x`, [`https://tomeraberba.ch/6?a=b#x`, 301]],
+
+  [`https://tomeraberba.ch/g`, [`https://tomeraberba.ch/7#x`, 301]],
+  [`https://tomeraberba.ch/g/`, [`https://tomeraberba.ch/7#x`, 301]],
+  [`https://tomeraberba.ch/g?x=y`, [`https://tomeraberba.ch/7?x=y#x`, 301]],
+  [`https://tomeraberba.ch/g/?x=y`, [`https://tomeraberba.ch/7?x=y#x`, 301]],
+  [`https://tomeraberba.ch/g#y`, [`https://tomeraberba.ch/7#y`, 301]],
+  [`https://tomeraberba.ch/g/#y`, [`https://tomeraberba.ch/7#y`, 301]],
+  [`https://tomeraberba.ch/g?x=y#y`, [`https://tomeraberba.ch/7?x=y#y`, 301]],
+  [`https://tomeraberba.ch/g/?x=y#y`, [`https://tomeraberba.ch/7?x=y#y`, 301]],
+] as const)(`createRedirectUrl - %p -> %p (%p)`, (url, result) => {
+  const redirectUrl = createRedirectUrl([
+    [`/`, `/0`],
+    [`/a/`, `/1`],
+    [`/a`, `/never1`],
+    [`/b`, `/2/`],
+    [`/b/`, `/never2`],
+    [`/c`, `/3`],
+    [`/d/`, `/4/`],
+    [`/e/:splat+`, `/5/:splat+`],
+    { from: `/x`, to: `/y`, status: 307 },
+    [`/f`, `/6?a=b`],
+    [`/g`, `/7/#x`],
+    [`/h`, `/8?a=b#x`],
+  ])
+
+  expect(redirectUrl(url)).toStrictEqual(createResult(result))
+})
+
+const createResult = (
+  result: readonly [string, number] | null,
+): RedirectUrlResult | null => result && { url: result[0], status: result[1] }
+
+test(`createRedirectUrl - default status`, () => {
+  const redirectUrl = createRedirectUrl([[`/`, `/hello`]], {
+    defaultStatus: 307,
+  })
+
+  expect(redirectUrl(`https://tomeraberba.ch`)).toStrictEqual({
+    url: `https://tomeraberba.ch/hello`,
+    status: 307,
+  })
+})
+
+test(`parseRedirectUrl - too few parts in line`, () => {
+  expect(() => parseRedirectUrl(`/`)).toThrow(new Error(`Bad part count:\n/`))
+})
+
+test(`parseRedirectUrl - too many parts in line`, () => {
+  expect(() => parseRedirectUrl(`/ /hello 307 HELLO`)).toThrow(
+    new Error(`Bad part count:\n/ /hello 307 HELLO`),
+  )
+})
+
+test(`parseRedirectUrl - bad status`, () => {
+  expect(() => parseRedirectUrl(`/ /hello howdy`)).toThrow(
+    new Error(`Bad status: howdy`),
+  )
+})
+
+test(`parseRedirectUrl - success`, () => {
+  const redirectUrl = parseRedirectUrl(`
+    /a /b
+    # comment
+
+    /c /d?x=2#x 307
+  `)
+
+  expect(redirectUrl(`https://tomeraberba.ch/a`)).toStrictEqual({
+    url: `https://tomeraberba.ch/b`,
+    status: 301,
+  })
+  expect(redirectUrl(`https://tomeraberba.ch/c`)).toStrictEqual({
+    url: `https://tomeraberba.ch/d?x=2#x`,
+    status: 307,
+  })
 })
